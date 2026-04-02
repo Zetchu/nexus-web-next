d3e6ab0f8ed14a460a242f8bf10984cf8cd822c9
Zetchu <shmungula@gmail.com>
2026-04-02 15:27:12 +0200

    refactor: update ESLint configuration to use flat config format and streamline global ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,40 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
-
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 
-const eslintConfig = defineConfig([
+export default [
+  // 1. GLOBAL IGNORES
+  // This must be its own object at the very top of the array to apply globally!
+  {
+    ignores: [
+      '.next/**',
+      'out/**',
+      'build/**',
+      'dist/**',
+      'node_modules/**',
+      'next-env.d.ts',
+    ],
+  },
+
+  // 2. Next.js & Storybook Presets
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  ...storybook.configs['flat/recommended'],
+
+  // 3. Custom Project Rules
   {
     rules: {
+      // Allow standard <img> tags since we use external Riot Data Dragon images
       '@next/next/no-img-element': 'off',
+
+      // Silence the warnings for the 'any' types we used in the API routes
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      // Clean up unused variable warnings so your terminal stays clean
+      '@typescript-eslint/no-unused-vars': 'off',
+
+      // Stop ESLint from yelling about unescaped apostrophes
+      'react/no-unescaped-entities': 'off',
     },
   },
-  ...storybook.configs['flat/recommended'],
-]);
-
-export default eslintConfig;
+];
